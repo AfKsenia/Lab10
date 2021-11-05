adf008602c96a34d4d3cbd0b5393671c2de69042
AfKsenia <afksenia@gmail.com>
2021-11-05 11:59:43 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle angle = new Angle(10, 10, 10);
+            angle.ToRadians();
+            Console.ReadKey();
+        }
+    }
+    class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+        double rad;
+
+        public int Gradus
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    gradus = value;
+                }
+                else
+                {
+                    Console.WriteLine("Значение должно быть положительным");
+                }
+            }
+            get
+            {
+                return gradus;
+            }
+        }
+        public int Min
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    min = value;
+                }
+                else
+                {
+                    Console.WriteLine("Значение должно быть положительным");
+                }
+            }
+            get
+            {
+                return min;
+            }
+        }
+        public int Sec
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    sec = value;
+                }
+                else
+                {
+                    Console.WriteLine("Значение должно быть положительным");
+                }
+            }
+            get
+            {
+                return sec;
+            }
+        }
+        public Angle(int gradus, int min, int sec)
+        {
+            Gradus = gradus;
+            Min = min;
+            Sec = sec;
+        }
+        public void ToRadians()
+        {
+            
+            rad = ((gradus + (min + sec / 60) / 60)* Math.PI) /180;
+            Console.WriteLine("{0} {1} {2} \nУгол в радианах {3}", gradus, min, sec, rad);
+        }
+
+    }
+}
